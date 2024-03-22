4d70bb01f255c4c1f5b64c1c415a6870e4336a74
John Bermeo <john.bermeo@gmail.com>
2024-03-22 20:32:41 +0100

    mn-04 dark mode implemented

--- src/app/core/components/main-header/main-header.component.ts
+++ src/app/core/components/main-header/main-header.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, Inject, Renderer2, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { SidenavService } from '../../services/Sidenav.service';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-main-header',
@@ -14,5 +15,14 @@ import { SidenavService } from '../../services/Sidenav.service';
   styleUrl: './main-header.component.scss'
 })
 export class MainHeaderComponent {
+  constructor(@Inject(DOCUMENT) private document: Document) {}
   public navService = inject(SidenavService);
+  private renderer = inject(Renderer2);
+  public isDarkMode = false;
+
+  public switchTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    if(this.isDarkMode) this.renderer.addClass(this.document.body, 'dark')
+    else this.renderer.removeClass(this.document.body, 'dark')
+  }
 }
