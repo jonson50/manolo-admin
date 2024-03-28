32fb782177cdcfa948efcf33b133eddf2000b5f5
John Bermeo <john.bermeo@gmail.com>
2024-03-28 09:41:09 +0100

    mn-06 analytics components created

--- src/app/modules/dashboards/analytics/analytics.component.ts
+++ src/app/modules/dashboards/analytics/analytics.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatRippleModule} from '@angular/material/core';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-analytics',
   standalone: true,
-  imports: [],
+  imports: [
+    MatMenuModule,
+    MatRippleModule
+  ],
   templateUrl: './analytics.component.html',
   styleUrl: './analytics.component.scss'
 })
